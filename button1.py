0b4def7ecf4a1a378f5747faea62996b697d3d30
mingijeon <mumu345@naver.com>
2017-11-15 14:22:08 +0900

    birthday

--- button1.py
+++ button1.py
@@ -4,8 +4,10 @@ window =Tk()
 
 b1 = Button(window, text="전민기")
 b2 = Button(window, text="60152236")
+b3 = Button(window, text="0803")
 
 b1.grid(row=0, column=0)
 b2.grid(row=1, column=1)
+b3.grid(row=1 ,column=0)
 
 window.mainloop()
