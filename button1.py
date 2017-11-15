c2da02334735a7b134af9f70e46285052b3c07f9
mingijeon <mumu345@naver.com>
2017-11-15 14:07:18 +0900

    name and student-id

--- button1.py
+++ button1.py
@@ -2,8 +2,8 @@ from tkinter import *
 
 window =Tk()
 
-b1 = Button(window, text="One")
-b2 = Button(window, text="Two")
+b1 = Button(window, text="전민기")
+b2 = Button(window, text="60152236")
 
 b1.grid(row=0, column=0)
 b2.grid(row=1, column=1)
